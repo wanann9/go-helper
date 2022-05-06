1ec09226930b21d594dbb31e178c21c93c029662
wanann9 <a18744447768@163.com>
2022-05-06 20:10:22 +0800

    add trie01

--- utils/contest/trie.go
+++ utils/contest/trie.go
@@ -79,3 +79,108 @@ func (t *trie) delete(s string, cnt int) (*trieNode, int) {
 	}
 	return p, cnt
 }
+
+type trieNode01 struct {
+	children       [2]*trieNode01
+	parent         *trieNode01
+	curCnt, subCnt int
+	//min            int
+}
+
+var trn01 = func(parent *trieNode01) *trieNode01 {
+	return &trieNode01{
+		parent: parent,
+		//min:    math.MaxInt,
+	}
+}
+
+type trie01 struct {
+	root *trieNode01
+	bits int
+}
+
+var tr01 = func(bits int) *trie01 {
+	return &trie01{
+		root: trn01(nil),
+		bits: bits,
+	}
+}
+
+func (t *trie01) _parse(n int) []int {
+	rst := vct(t.bits, 0)
+	for i := t.bits - 1; n > 0; n, i = n>>1, i-1 {
+		rst[i] = n & 1
+	}
+	return rst
+}
+
+func (t *trie01) insert(n, cnt int) *trieNode01 {
+	p := t.root
+	for _, i := range t._parse(n) {
+		if p.subCnt += cnt; p.children[i] == nil {
+			p.children[i] = trn01(p)
+		}
+		p = p.children[i]
+		//p.min = min(p.min, n)
+	}
+	p.curCnt += cnt
+	return p
+}
+
+func (t *trie01) search(n int) *trieNode01 {
+	p := t.root
+	for _, i := range t._parse(n) {
+		if p = p.children[i]; p == nil {
+			return nil
+		}
+	}
+	return p
+}
+
+func (t *trie01) delete(n, cnt int) (*trieNode01, int) {
+	p := t.search(n)
+	if p == nil {
+		return nil, 0
+	}
+	if cnt < 0 || cnt > p.curCnt {
+		cnt = p.curCnt
+	}
+	if cnt == 0 {
+		return p, 0
+	}
+	p.curCnt -= cnt
+	for q := p.parent; q != nil; q = q.parent {
+		q.subCnt -= cnt
+	}
+	return p, cnt
+}
+
+func (t *trie01) query(n int) (p *trieNode01, a int) {
+	if p, a = t.root, 0; p.subCnt == 0 {
+		return nil, 0
+	}
+	for _, i := range t._parse(n) {
+		j := i ^ 1
+		if q := p.children[j]; q == nil || q.curCnt+q.subCnt == 0 {
+			j = i
+		}
+		p, a = p.children[j], a<<1+j
+	}
+	return
+}
+
+//func (t *trie01) query2(n, m int) (p *trieNode01, a int) {
+//	if p, a = t.root, 0; p.subCnt == 0 {
+//		return nil, 0
+//	}
+//	for _, i := range t._parse(n) {
+//		j := i ^ 1
+//		if q := p.children[j]; q == nil || q.min > m {
+//			if j = i; p.children[j].min > m {
+//				return nil, 0
+//			}
+//		}
+//		p, a = p.children[j], a<<1+j
+//	}
+//	return
+//}
--- utils/contest/trie_test.go
+++ utils/contest/trie_test.go
@@ -14,3 +14,15 @@ func Test_trie(t *testing.T) {
 	t.Log(trie.search("ab"))
 	t.Log(trie.delete("ab", 1))
 }
+
+func Test_trie01(t *testing.T) {
+	trie := tr01(2)
+	trie.insert(0, 1)
+	trie.insert(1, 1)
+	trie.insert(2, 1)
+	trie.insert(3, 1)
+	t.Log(trie.query(0))
+	t.Log(trie.query(1))
+	t.Log(trie.query(2))
+	t.Log(trie.query(3))
+}
