a303d7b520ed78fdac55f7e76dacdcb7109a77d3
wanann9 <a18744447768@163.com>
2022-05-06 15:54:26 +0800

    add segment tree

---
+++ utils/contest/segment_tree.go
@@ -0,0 +1,85 @@
+package contest
+
+type segmentTree struct {
+	n     int
+	a     []int
+	nodes []smtNode
+}
+
+type smtNode struct {
+	f, v int
+}
+
+var smt = func(a []int) *segmentTree {
+	n := len(a)
+	t := &segmentTree{
+		n:     n,
+		a:     a,
+		nodes: make([]smtNode, n<<2+1),
+	}
+	t._build(1, 0, n-1)
+	return t
+}
+
+func (t *segmentTree) _build(i, l, r int) {
+	if l == r {
+		t.nodes[i].f = t.a[l]
+		return
+	}
+	m, i1, i2 := l+(r-l)>>1, i<<1, i<<1+1
+	t._build(i1, l, m)
+	t._build(i2, m+1, r)
+	t.nodes[i].f = t.nodes[i1].f + t.nodes[i2].f
+}
+
+func (t *segmentTree) update(x, y, k int) {
+	t._update(1, 0, t.n-1, x, y, k)
+}
+
+func (t *segmentTree) _update(i, l, r, x, y, k int) {
+	if x <= l && y >= r {
+		t.nodes[i].v += k
+		return
+	}
+	m, i1, i2 := l+(r-l)>>1, i<<1, i<<1+1
+	t._down(i, i1, i2, l, m, r)
+	if x <= m {
+		t._update(i1, l, m, x, y, k)
+	}
+	if y > m {
+		t._update(i2, m+1, r, x, y, k)
+	}
+	t._up(i, i1, i2, l, m, r)
+}
+
+func (t *segmentTree) calc(x, y int) int {
+	return t._calc(1, 0, t.n-1, x, y)
+}
+
+func (t *segmentTree) _calc(i, l, r, x, y int) (rst int) {
+	if x <= l && y >= r {
+		return t.nodes[i].f + t.nodes[i].v*(r-l+1)
+	}
+	m, i1, i2 := l+(r-l)>>1, i<<1, i<<1+1
+	t._down(i, i1, i2, l, m, r)
+	if x <= m {
+		rst += t._calc(i1, l, m, x, y)
+	}
+	if y > m {
+		rst += t._calc(i2, m+1, r, x, y)
+	}
+	t._up(i, i1, i2, l, m, r)
+	return
+}
+
+func (t *segmentTree) _down(i, i1, i2, l, m, r int) {
+	if v := t.nodes[i].v; v != 0 {
+		t.nodes[i1].v += v
+		t.nodes[i2].v += v
+		t.nodes[i].v = 0
+	}
+}
+
+func (t *segmentTree) _up(i, i1, i2, l, m, r int) {
+	t.nodes[i].f = t.nodes[i1].f + t.nodes[i1].v*(m-l+1) + t.nodes[i2].f + t.nodes[i2].v*(r-m)
+}
