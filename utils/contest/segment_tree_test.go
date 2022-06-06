a69a658ac32ccb49798aea7ab9bbb51741f4b9cd
wanann9 <a18744447768@163.com>
2022-06-06 17:50:12 +0800

    add dynamic segment tree

--- utils/contest/segment_tree_test.go
+++ utils/contest/segment_tree_test.go
@@ -12,3 +12,19 @@ func Test_segmentTree(t *testing.T) {
 	t.Log(tree.calc(1, 3))
 	t.Log(tree)
 }
+
+func Test_segmentTree2(t *testing.T) {
+	tree := smt2(0, 4)
+	tree.update(0, 4, 1)
+	tree.update(1, 4, 1)
+	tree.update(2, 4, 1)
+	tree.update(3, 4, 1)
+	tree.update(4, 4, 1)
+	t.Log(tree.smtNode2)
+	tree.update(0, 4, 1)
+	t.Log(tree.smtNode2)
+	tree.update(0, 2, 1)
+	t.Log(tree.smtNode2)
+	t.Log(tree.calc(1, 3))
+	t.Log(tree.smtNode2)
+}
