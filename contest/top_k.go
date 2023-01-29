bf1494d3ba7228e82022ea1f2f5248a8ff5968b0
mengxiangzhuo.0 <mengxiangzhuo.0@bytedance.com>
2023-01-29 14:54:25 +0800

    fix top k

--- contest/top_k.go
+++ contest/top_k.go
@@ -17,6 +17,9 @@ func tk(k int, cmp utils.Comparator) *topK {
 }
 
 func (t *topK) Push(items ...interface{}) {
+	if t.k <= 0 {
+		return
+	}
 	for _, item := range items {
 		if t.Size() < t.k || t.cmp(item, t.Peek()) > 0 {
 			if t.heap.Push(item); t.Size() > t.k {
